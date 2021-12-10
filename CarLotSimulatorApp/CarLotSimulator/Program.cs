d547123d9f37b5e7eaa488be10c6fa2a2f13996a
Aaron <git@github.com:aromero082/PizzaAlgo.git>
2021-12-09 18:02:00 -0800

    CarLotSimulator Exercise2

--- CarLotSimulatorApp/CarLotSimulator/Program.cs
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -4,6 +4,18 @@ namespace CarLotSimulator
 {
     class Program
     {
+        private static string vroom;
+        private static string beep;
+
+        public static object Year { get; private set; }
+        public static object Model { get; private set; }
+        public static string EngineNoise { get; private set; }
+        public static string HonkNoise { get; private set; }
+        public static bool IsDriveable { get; private set; }
+        public static object Make { get; private set; }
+        public static string Nissan { get; private set; }
+        public static string Rougue { get; private set; }
+
         static void Main(string[] args)
         {
             //TODO
@@ -17,6 +29,36 @@ namespace CarLotSimulator
             //Now that the Car class is created we can instanciate 3 new cars
             //Set the properties for each of the cars
             //Call each of the methods for each car
+            //Instantiation 1
+            var aaronsCar = new Car();
+            aaronsCar.Make = "Toyota";
+            aaronsCar.Model = "Tundra";
+            aaronsCar.Year = 2015;
+            aaronsCar.EngineNoise = "vroooom";
+            aaronsCar.HonkNoise = "boom boom";
+            aaronsCar.IsDriveable = true;
+            //Instantiation 2
+            var ashleyscar = new Car();
+            {
+                Year = 2015;
+                Make = "Honda";
+                Model = "Civic";
+                EngineNoise = "vreem";
+                HonkNoise = "beep beep";
+                IsDriveable = true;
+              
+            }
+            //Instantiation 3
+            var navysCar = new Car(2016, Nissan, Rougue, vroom, beep, true);
+
+            //Call the methods
+            aaronsCar.MakeEngineNoise();
+            ashleyscar.MakeEngineNoise();
+            navysCar.MakeEngineNoise();
+
+            
+
+
 
             //*************BONUS*************//
 
